e8cb06c3a4f5fa9904729300f1460c63a90c4e4c
rentianzhu <rentianzhu@bytedance.com>
2023-10-06 22:10:44 +0800

    count

--- src/comment.tsx
+++ src/comment.tsx
@@ -7,13 +7,30 @@ import DataComment from "./datastruct.tsx";
  */
 function Comment({dataComment: dataComment}) {
     const [showSubmit, setShowSubmit] = useState<Boolean>(false)
-    const [likeCount, setLikeCount] = useState<Number>(0)
+    const [likeCount, setLikeCount] = useState<number>(0)
     const [comments, setComments] = useState<DataComment[]>([])
     return (
         <div className="shadow-md p-4">
-            <div className="text-sky-400 break-words mt-2">{dataComment.username}</div>
-            <div className="break-all">{dataComment.content}</div>
+            <div className="flex flex-row place-items-start justify-between">
+                <div>
+                    <div className="text-sky-400 break-words mt-2">{dataComment.username}</div>
+                    <div className="break-all">{dataComment.content}</div>
+                </div>
+                <div className="flex flex-col items-center p-4">
+                    <button className="text-green-500"
+                            onClick={() => setLikeCount(likeCount + 1)}
+                    >+
+                    </button>
+                    <h1>{likeCount}</h1>
+                    <button className="text-red-500"
+                            onClick={() => setLikeCount(likeCount - 1)}
+                    >-
+                    </button>
+                </div>
+            </div>
+
             <button
+                className="text-white bg-green-500 hover:bg-green-700 px-2 py-2 ml-auto rounded"
                 name={"Comment"}
                 onClick={() => {
                     setShowSubmit(!showSubmit)
